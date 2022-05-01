b83a227f32200d7342465085de7857261d67b156
fazruslan <fazylyanovr@bk.ru>
2022-05-01 10:58:56 +0300

    add api test base

---
+++ ReqresApiTesting/ApiTestBase.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace ReqresApiTesting;
+
+public class ApiTestBase
+{
+    private static string URI = "https://reqres.in";
+    
+    protected static RestRequest AddRequestHeaders(RestRequest request)
+    {
+        // тут можно задавать хедеры необходимые для запроса, либо, как пример, проходить авторизацию чтобы получить токен/куки
+        // в случае с Reqres можно вообще ничего указывать, просто как пример
+        
+        request.AddHeader("Accept", "appplication/json");
+        
+        return request;
+    }
+
+    protected static RestResponse SendRequestAndGetResponse(RestRequest request)
+    {
+        var restClient = new RestClient(URI);
+        
+        RestResponse response = restClient.Execute(request);
+
+        return response;
+    }
+
+    protected static JObject GetResponseContent(RestResponse response)
+    {
+        JObject json = JObject.Parse(response.Content);
+        return json;
+    }
+}
