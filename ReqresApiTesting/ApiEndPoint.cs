cc65fa32346f85e183cfafefb5996379a57cddd5
fazruslan <fazylyanovr@bk.ru>
2022-05-01 10:47:04 +0300

    add apiendpint

---
+++ ReqresApiTesting/ApiEndPoint.cs
@@ -0,0 +1,8 @@
+﻿namespace ReqresApiTesting;
+
+public class ApiEndPoint
+{
+    public static string GetSingleUser => "/api/users";
+    public static string CreateUser => "/api/users";
+    public static string LoginUser => "/api/login";
+}
