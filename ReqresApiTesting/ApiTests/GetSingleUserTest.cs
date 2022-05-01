b5f47f40ecace6bd5345342dcfaf9869df4d458e
fazruslan <fazylyanovr@bk.ru>
2022-05-01 11:08:43 +0300

    add get single user tests

---
+++ ReqresApiTesting/ApiTests/GetSingleUserTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using RestSharp;
+
+namespace ReqresApiTesting.ApiTests;
+
+public class GetSingleUserTest : ApiTestBase
+{
+    
+    [Test]
+    public void GetSingleUserPositiveTesting()
+    {
+        int someUserId = 3;
+        
+        RestRequest request = new RestRequest(ApiEndPoint.GetSingleUser + $"/{someUserId}", Method.Get);
+        request = AddRequestHeaders(request);
+
+        var response = SendRequestAndGetResponse(request);
+        var content = GetResponseContent(response);
+
+        Assert.AreEqual(200, (int)response.StatusCode, "Статус код ответа не соответствует ожидаемому");
+        Assert.AreEqual(someUserId, (int)content["data"]?["id"], "Данные не по заданному пользователю");
+    }
+
+    [Test]
+    public void GetSingleUserNegativeTesting()
+    {
+        int someNotExistUserId = 99999999;
+        
+        RestRequest request = new RestRequest(ApiEndPoint.GetSingleUser + $"/{someNotExistUserId}", Method.Get);
+        request = AddRequestHeaders(request);
+
+        var response = SendRequestAndGetResponse(request);
+        var content = GetResponseContent(response);
+        
+        Assert.AreEqual(404, (int)response.StatusCode, "Статус код ответа не соответствует ожидаемому");
+        Assert.IsEmpty(content, "Тело ответа не пустое.");
+    }
+}
