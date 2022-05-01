2813ac65fe486d877ea0c7a5836a47e258278871
fazruslan <fazylyanovr@bk.ru>
2022-05-01 11:25:11 +0300

    add login user tests

---
+++ ReqresApiTesting/ApiTests/CreateUserTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using RestSharp;
+
+namespace ReqresApiTesting.ApiTests;
+
+public class CreateUserTest : ApiTestBase
+{
+    [Test]
+    public void CreateUserTesting()
+    {
+        string someName = "someName";
+        string someJob = "someJob";
+        
+        RestRequest request = new RestRequest(ApiEndPoint.CreateUser, Method.Post);
+        request = AddRequestHeaders(request);
+        request.AddParameter("name", someName);
+        request.AddParameter("job", someJob);
+
+        var response = SendRequestAndGetResponse(request);
+        var content = GetResponseContent(response);
+        
+        Assert.AreEqual(201, (int)response.StatusCode);
+        Assert.AreEqual(someName, content["name"].ToString(), 
+            "Имя пользователя отправленного в запросе != имени созданного пользователя");
+        Assert.AreEqual(someJob, content["job"].ToString(),
+            "Наименование работы отправленного в запросе != наименованию работы созданного пользователя");
+        
+        // при отправке этого запроса пользователь на самом деле не создается
+        // если отправить запрос на получение данных о пользователя, то получим Not Found
+        // поэтому нет возможности сделать проверку на то, действительно ли был создан пользователь
+    }
+}
---
+++ ReqresApiTesting/ApiTests/LoginUserTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using RestSharp;
+
+namespace ReqresApiTesting.ApiTests;
+
+public class LoginUserTest : ApiTestBase
+{
+    // данные для авторизации взяты с самого примера на reqres.in
+
+    private const string Email = "eve.holt@reqres.in";
+    private const string Password = "cityslicka";
+    private const string MissingEmailError = "Missing email or username";
+    private const string MissingPasswordError = "Missing password";
+    
+    [Test]
+    public void SuccessfulUserLoginTesting()
+    {
+        
+        RestRequest request = new RestRequest(ApiEndPoint.LoginUser, Method.Post);
+        request = AddRequestHeaders(request);
+        request.AddParameter("email", Email);
+        request.AddParameter("password", Password);
+
+        var response = SendRequestAndGetResponse(request);
+        var content = GetResponseContent(response);
+        
+        Assert.AreEqual(200, (int)response.StatusCode, "Статус код ответа не соответствует ожидаемому");
+        Assert.IsNotEmpty(content["token"].ToString(), "Token is empty");
+    }
+
+    [Test]
+    public void LoginUserWithMissingEmailTesting()
+    {
+        RestRequest request = new RestRequest(ApiEndPoint.LoginUser, Method.Post);
+        request = AddRequestHeaders(request); 
+        request.AddParameter("password", Password);
+
+        var response = SendRequestAndGetResponse(request);
+        var content = GetResponseContent(response);
+
+        Assert.AreEqual(400, (int)response.StatusCode, "Статус код ответа не соответствует ожидаемому");
+        Assert.AreEqual(MissingEmailError, content["error"].ToString(), "Текст ошибки в ответе api != ожидаемому");
+    }
+    
+    [Test]
+    public void LoginUserWithMissingPasswordTesting()
+    {
+        RestRequest request = new RestRequest(ApiEndPoint.LoginUser, Method.Post);
+        request = AddRequestHeaders(request); 
+        request.AddParameter("email", Email);
+
+        var response = SendRequestAndGetResponse(request);
+        var content = GetResponseContent(response);
+
+        Assert.AreEqual(400, (int)response.StatusCode, "Статус код ответа не соответствует ожидаемому");
+        Assert.AreEqual(MissingPasswordError, content["error"].ToString(), "Текст ошибки в ответе api != ожидаемому");
+    }
+}
